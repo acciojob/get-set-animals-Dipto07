4b2b6730eab9d13e78b86f5cd9404859abc208ff
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-28 04:38:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -37,6 +37,24 @@ class Cat extends Animal {
     }
 }
 
+describe('Animal Classes', () => {
+    beforeEach(() => {
+        cy.visit(baseUrl + "/main.html");
+    });
+
+    it('should make sound and bark', () => {
+        cy.window().then(win => {
+            const DogClass = win.Dog;
+            const species = "Golden Retriever";
+            const myDog = new DogClass(species);
+            cy.stub(win.console, "log").as("consoleLog");
+            myDog.makeSound();
+            cy.get("@consoleLog").should("be.calledWith", `The ${species} makes a sound`);
+            myDog.bark();
+            cy.get("@consoleLog").should("be.calledWith", `woof`);
+        });
+    });
+});
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
